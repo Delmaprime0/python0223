01b1ae9e026276d911bd7fe9a6d506b3a3658a7d
Eduardo Martinez <santos.martinezochoa@gmail.com>
2023-03-11 22:56:53 -0500

    actualizacion3

--- Clase2EjerciciosResueltos/Ejercicio6.py
+++ Clase2EjerciciosResueltos/Ejercicio6.py
@@ -1,20 +1,20 @@
 # Crear una funcion Main que valide el ingreso para un evento, dividir esta funcion main en al menos 3 sub-funciones
 # SANTOS EDUARDO MARTINEZ OCHOA
 def main():
-    evento = obtener_evento()
-    validar_evento(evento)
-    procesar_evento(evento)
+    evento = obtEvent()
+    validar(evento)
+    procesar(evento)
 
-def obtener_evento():
-    evento = input("Ingrese el nombre del evento: ")
+def obtEvent():
+    evento = input("Digite el nombre del evento: ")
     return evento
 
-def validar_evento(evento):
+def validar(evento):
     eventos_validos = ['fiesta', 'concierto', 'obra de teatro']
     if evento.lower() not in eventos_validos:
-        raise ValueError("El evento ingresado no es válido.")
+        raise ValueError("El evento digitado no es válido.")
 
-def procesar_evento(evento):
-    print(f"Procesando el evento {evento}...")
+def procesar(evento):
+    print(f"Evento {evento}...")
 
 main()
