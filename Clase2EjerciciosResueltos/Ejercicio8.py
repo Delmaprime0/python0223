b36dc4a8cedad0d2047e52c3699b2777ee21809e
Eduardo Martinez <santos.martinezochoa@gmail.com>
2023-03-11 22:52:53 -0500

    si

--- Clase2EjerciciosResueltos/Ejercicio8.py
+++ Clase2EjerciciosResueltos/Ejercicio8.py
@@ -1,7 +1,7 @@
 # Generar una funcion rango hasta un numero maximo con un step y agregar a una lista los numeros que cumplan las siguientes opciones, que se primos 
 # SANTOS EDUARDO MARTINEZ OCHOA
 
-def primes_in_range(start, end, step):
+def primosRange(start, end, step):
     primes = []
     for num in range(start, end+1, step):
         if num > 1:
@@ -11,5 +11,5 @@ def primes_in_range(start, end, step):
             else:
                 primes.append(num)
     return primes
-primes = primes_in_range(0, 10, 1)
+primes = primosRange(0, 10, 1)
 print(primes)
